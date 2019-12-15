96f173328d12257ccf9160c99052d46690040e94
Mel Bro <melbournebro1@gmail.com>
2019-12-15 23:02:28 +1100

    feat: complete popup logic

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,19 +1,58 @@
 <template>
-  <hello-world />
+  <section class="section">
+    <div v-if="playbackRates.length" class="container">
+      <b-field
+        v-for="(playbackRate, idx) in playbackRates"
+        :key="idx"
+        :label="`Video ${idx + 1} Speed: ${playbackRate}x`">
+        <b-slider
+          v-model="playbackRates[idx]"
+          size="is-large"
+          :min="MIN"
+          :max="MAX"
+          :step="STEP"
+          @change="value => { setVideoPlaybackRate(idx, value) }">
+        </b-slider>
+      </b-field>
+    </div>
+    <p v-else class="has-text-centered is-italic">
+      No videos found
+    </p>
+  </section>
 </template>
-
 <script>
-import HelloWorld from '@/components/HelloWorld.vue'
+import { GET_VIDEOS_PLAYBACK_RATE, SET_VIDEO_PLAYBACK_RATE } from '@/content-scripts/actions/videosPlaybackRate'
+import getCurrentTabId from './utils/getCurrentTabId'
 
 export default {
   name: 'App',
-  components: { HelloWorld }
+  data: () => ({
+    MIN: 0.25,
+    MAX: 5,
+    STEP: 0.25,
+
+    curTabId: null, /** number */
+    playbackRates: [] /** number[] */
+  }),
+  async mounted () {
+    this.curTabId = await getCurrentTabId()
+    const resp /** number[] */ = await browser.tabs.sendMessage(this.curTabId, {
+      action: GET_VIDEOS_PLAYBACK_RATE
+    })
+    this.playbackRates = resp
+  },
+  methods: {
+    /**
+     * @param {number} idx
+     * @param {number} value
+     */
+    setVideoPlaybackRate (idx, value) {
+      browser.tabs.sendMessage(this.curTabId, {
+        action: SET_VIDEO_PLAYBACK_RATE,
+        idx,
+        value
+      })
+    }
+  }
 }
 </script>
-
-<style>
-html {
-  width: 400px;
-  height: 400px;
-}
-</style>
---
+++ src/popup/utils/getCurrentTabId.js
@@ -0,0 +1,10 @@
+/**
+ * @return {Promise<number>}
+ */
+export default async function () {
+  const tabs = await browser.tabs.query({ active: true, currentWindow: true })
+  if (!tabs.length) {
+    throw new Error('No active tab available')
+  }
+  return tabs[0].id
+}
