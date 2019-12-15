10cc820de3fed5661ca0fa29d89a62817c42037a
Mel Bro <melbournebro1@gmail.com>
2019-12-15 22:53:19 +1100

    feat: add content-script

---
+++ src/content-scripts/actions/videosPlaybackRate.js
@@ -0,0 +1,23 @@
+function $$ (selector) {
+  return document.querySelectorAll(selector)
+}
+
+/**
+ * @return {number[]}
+ */
+export function getVideosPlaybackRate () {
+  const videos = $$('video')
+  return videos.length
+    ? Array.from(videos).map(el => el.playbackRate)
+    : []
+}
+export const GET_VIDEOS_PLAYBACK_RATE = 'GET_VIDEOS_PLAYBACK_RATE'
+
+/**
+ * @param {number} idx
+ * @param {number} value
+ */
+export function setVideoPlaybackRate (idx, value) {
+  $$('video')[idx].playbackRate = value
+}
+export const SET_VIDEO_PLAYBACK_RATE = 'SET_VIDEO_PLAYBACK_RATE'
--- src/content-scripts/content-script.js
+++ src/content-scripts/content-script.js
@@ -1,1 +1,19 @@
-console.log('Hello from the content-script')
+import {
+  GET_VIDEOS_PLAYBACK_RATE,
+  getVideosPlaybackRate,
+  SET_VIDEO_PLAYBACK_RATE,
+  setVideoPlaybackRate
+} from './actions/videosPlaybackRate'
+
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  switch (request.action) {
+    case GET_VIDEOS_PLAYBACK_RATE:
+      sendResponse(getVideosPlaybackRate())
+      return
+    case SET_VIDEO_PLAYBACK_RATE:
+      const { idx, value } = request
+      setVideoPlaybackRate(idx, value)
+      return
+  }
+  throw new Error('Unhandled request: ' + JSON.stringify(request))
+})
