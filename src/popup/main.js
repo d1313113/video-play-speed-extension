2adad6453851ae922b3975ad98325201288230b2
Mel Bro <melbournebro1@gmail.com>
2019-12-15 22:51:18 +1100

    style: setup Buefy

--- src/popup/main.js
+++ src/popup/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
+import { Field, Slider } from 'buefy'
+import 'buefy/dist/buefy.css'
 import App from './App.vue'
+
+Vue.use(Field)
+Vue.use(Slider)
 
 /* eslint-disable no-new */
 new Vue({
